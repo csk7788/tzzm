679eada50d4ef98de5efc5b2d78fffeb9e2ab290
程少康 <chengshaokang@baiwang.com>
2022-01-02 15:04:53 +0800

    test

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,10 +1,10 @@
 public class Test2 {
 
 
-        public static void main(String[] args) {
+public static void main(String[] args) {
 
-            System.out.println(333);
-            System.out.println(444);
-        }
-    }
+System.out.println(333);
+System.out.println(444);
+}
+}
 
